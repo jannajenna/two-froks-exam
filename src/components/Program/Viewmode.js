c39ff3d3a720e1b46fbf84f6737bdcfc6c8bdda4
anna <annabarellaalonso@gmail.com>
2023-08-15 23:54:35 +0200

    cleaned data

--- src/components/Program/Viewmode.js
+++ src/components/Program/Viewmode.js
@@ -19,26 +19,3 @@ export default function Viewmode() {
         </div>
     );
 }
-
-
-/* return (
-    <div className={styles.flex_row_icons}>
-
-        <FaListUl className={styles.icon_size} onClick={() => handleviewBUttonClick("LIST")} />
-        <BsFillGridFill className={styles.icon_size}
-            onClick={() => handleviewBUttonClick("CARD")} />
-        <AiFillStar className={`${styles.icon_size} ${styles.icon_list}`} />
-    </div>
-); */
-
-/* return (
-    <div className={styles.flex_row_icons}>
-        <Link href="/program/list" className={styles.color_icon}>
-            <FaListUl className={styles.icon_size} />
-        </Link>
-        <Link href="/program" className={styles.color_icon}>
-            <BsFillGridFill className={styles.icon_size} />
-        </Link>
-        <AiFillStar className={`${styles.icon_size} ${styles.icon_list}`} />
-    </div>
-); */
